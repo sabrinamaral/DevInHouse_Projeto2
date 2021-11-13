b790d56fe9033fe7210e5c2e6ba13af4e3ea658b
sabrinamaral <sabrinamaral@hotmail.com>
2021-11-13 16:55:59 -0300

    added routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { GameList } from './pages/GameList';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <GameList/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './index.css';
+import { Home } from './pages/Home'
+import { GameDetails } from './pages/GameDetails';
+import { GameList } from './pages/GameList';
+import { GameNews } from './pages/GameNews';
+import { GameContext } from './Context/game.context';
+
+const Index = () => {
+  return(
+    <GameContext>
+      <BrowserRouter>
+        <Routes>
+        <Route path='/' element={<Home />} />
+          <Route path='/gamelist' element={<GameList />} />
+          <Route path='/gameList/:gameId' element={<GameDetails />} />
+          <Route path='/gamenews' element={<GameNews />} />
+        </Routes>
+      </BrowserRouter>
+    </GameContext>
+  )
+}
+
+
+
+ReactDOM.render(<Index />, document.getElementById('root'));
 
