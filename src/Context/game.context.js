c2e3351eff1564542e3bb26031c5d4d64f6c277d
sabrinamaral <sabrinamaral@hotmail.com>
2021-11-15 23:01:28 -0300

    changed useState for useReducer

--- src/Context/game.context.js
+++ src/Context/game.context.js
@@ -1,27 +1,42 @@
-import React, { createContext, useState, useEffect } from "react";
-
+import React, { createContext, useReducer, useEffect } from "react";
 
 export const gameContext = createContext();
 gameContext.displayName = 'Game Context';
 const GameProvider = gameContext.Provider;
 
-export const GameContext = ({children}) => {
-    const [ games, setGames ] = useState([]);
+const initial_state = {
+    games: [],
+    game: null,
+    news: [],
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_GAMES':
+            return { ...state, games: action.content};
+        case 'SET_NEWS':
+            return {...state, news: action.content};
+        case 'SET_GAME':
+            return {...state, game: action.content};
+        default:
+            return state;
+    }
+};
+
+export const GameContext = ({ children }) => {
+
+const [state, dispatch] = useReducer(reducer, initial_state);
 
     useEffect(() => {
-        const config = {
+        fetch("https://mmo-games.p.rapidapi.com/games", {
             "method": "GET",
             "headers": {
                 "x-rapidapi-host": "mmo-games.p.rapidapi.com",
                 "x-rapidapi-key": process.env.REACT_APP_API_KEY
             }
-        } 
-        fetch("https://mmo-games.p.rapidapi.com/games", config)
-        .then(response=>response.json())
-        .then(data => setGames(data))
+        } )
+        .then(response=>response.json()).then(games => dispatch({ type: 'SET_GAMES', content: games}))
     }, []);
-
-    const [ news, setNews ] = useState([]);
 
     useEffect(() => {
         fetch("https://mmo-games.p.rapidapi.com/latestnews", {
@@ -31,12 +46,27 @@ export const GameContext = ({children}) => {
 		"x-rapidapi-key": process.env.REACT_APP_API_KEY
 	}
 })
-.then(response => response.json()).then(data => setNews(data))
-    }, [])
+.then(response => response.json()).then(news => dispatch({type: 'SET_NEWS', content: news}))
+    }, []);
+
+    const fetchGameId = (id) => {
+        if(!id) return;
+        
+       fetch(`https://mmo-games.p.rapidapi.com/game?id=${id}`, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "mmo-games.p.rapidapi.com",
+                "x-rapidapi-key": process.env.REACT_APP_API_KEY 
+        }}).then(response => response.json()).then(game => dispatch({type: 'SET_GAME', content: game}))
+    };
+
+    const clearGame = () => {
+        dispatch({ type: "SET_GAME", content: null });
+      };
 
     return (
         
-        <GameProvider value={{games, news}}>
+        <GameProvider value={{state, fetchGameId, clearGame}}>
             {children}
         </GameProvider>
     )
