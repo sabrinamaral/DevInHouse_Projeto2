b66e3062bbbe6b8faf43d3c83736fc4fb7c2d5d1
sabrinamaral <sabrinamaral@hotmail.com>
2021-11-20 20:46:17 -0300

    search bar in news implemented

--- src/pages/GameNews.js
+++ src/pages/GameNews.js
@@ -1,27 +1,38 @@
 import { useContext } from "react";
 
 import { SectionNews } from "../components/News/News";
+import { Search } from "../components/Search/Search";
 import { gameContext } from "../context/game.context";
-import { StyledContainer } from "./gameNews.styles";
+import { StyledContainer, StyledSpan } from "./gameNews.styles";
 
 export const GameNews = () => {
-    const { state } = useContext(gameContext);
-    const { news } = state;
+    const { state, handleChange } = useContext(gameContext);
+    const { news, searchResultsNews, searchTerm } = state;
+
+    const searchLength = searchResultsNews.length > 0 ? searchResultsNews.length : news.length;
+    const newsFiltered = searchResultsNews.length > 0 ? searchResultsNews : news;
 
     return(
-        <>       
+        <>  
             <StyledContainer>
+            <StyledSpan> Notícias: {searchLength}</StyledSpan>
+            <Search 
+                type="search"
+                placeholder={"Busque pelo título da notícia."}
+                value={searchTerm}
+                onChange={handleChange}
+            />
                 {news.length > 0 ? (
-                    news.map(item => (
+                    newsFiltered.map(item => (
                         <SectionNews
                         key={item.id}
                         image={item.main_image}
                         url={item.article_url}
                         title={item.title}
                         description={item.short_description}
-                        />
+                        />  
                     ))
-                ) : (<p>Buscando notícias!</p>)}  
+                ) : (<p>Buscando notícias!</p>)} 
             </StyledContainer> 
         </>
     )
--- src/pages/gameNews.styles.js
+++ src/pages/gameNews.styles.js
@@ -1,4 +1,15 @@
 import styled from "styled-components";
 
 export const StyledContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const StyledSpan = styled.span`
+    align-self: flex-start;
+    margin-left: 4.5rem;
+    margin-top: 1rem;
+    height: 2rem;
+    text-transform: uppercase;
 `;
